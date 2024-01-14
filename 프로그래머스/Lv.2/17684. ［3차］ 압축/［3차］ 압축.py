3c63bb1aebccd1644189efddf3168b2f34a37276
Bokyeong Ju <87298145+jupyter1234@users.noreply.github.com>
2024-01-14 23:41:53 +0900

    [level Lv.2] Title: [3차] 압축, Time: 0.44 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Lv.2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/Lv.2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -5,6 +5,7 @@ def solution(msg):
     for i in range(ord('A'),ord('Z')+1):
         dic[chr(i)] = index
         index += 1
+        
     size = len(msg)
     p = 0 # W위치
     msg_len = len(msg)
@@ -12,11 +13,9 @@ def solution(msg):
         find_len = 1 #사전에서 찾을 문자열의 길이
         while(find_len <= msg_len):
             if msg[p:p + find_len] in dic:
-                #print(msg[p:p+find_len])
                 find_len += 1
             else:
                 find_len = (1 if find_len == 1 else find_len - 1)
-                #print("find",find_len)
                 break
         answer.append(dic[msg[p:p +find_len]])
         dic[msg[p:p + find_len+1]] = index
