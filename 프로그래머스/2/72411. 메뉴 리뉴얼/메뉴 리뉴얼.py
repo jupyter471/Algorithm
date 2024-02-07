e36fe27878c64d36a235963a6418817f65ba6bb5
Bokyeong Ju <87298145+jupyter1234@users.noreply.github.com>
2024-02-08 01:15:31 +0900

    Update 메뉴 리뉴얼.py

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -14,8 +14,8 @@ def solution(orders, course):
                 else:
                     dict[case] = 1
         print(dict)
-        temp = [k for k,v in dict.items() if (v >= 2 and v == max(dict.values()))]
-        answer += temp
+        temp = [k for k,v in dict.items() if (v >= 2 and v == max(dict.values()))] #Counter().most_common() 사용시 최대값 반환해줌
+        answer += temp  #append가 아니라 + 를 해야 원소값들끼리 저장됨!
     return sorted(answer)
 #course : 2부터 10까지 오름차순
 #answer : 각 course에 따른 메뉴 조합 오름차순
