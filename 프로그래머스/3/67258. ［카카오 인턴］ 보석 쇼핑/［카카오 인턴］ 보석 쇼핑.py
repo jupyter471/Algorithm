3c67ae94ff324d0a3793058a1b9c3967c4a43bd1
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2024-10-19 02:44:55 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 85.32 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,45 +1,28 @@
-#2시 25분
+from collections import defaultdict
 def solution(gems):
-    part = [] #구간
-    cnt = len(set(gems)) #종류 개수
-    end = 0
-    ishere = {}
-    gcnt = 0
-    for start,g in enumerate(gems):
-        # print(start)
-        #end 이동
-        while len(ishere) < cnt and end < len(gems):
-            if gems[end] not in ishere:
-                ishere[gems[end]] = 1
-            else:
-                ishere[gems[end]] += 1
-            gcnt += 1
+    answer = [1, len(gems)]
+    
+    kind = len(set(gems))
+    count = defaultdict(int)
+    start = end = 0
+    count[gems[0]] = 1
+    
+    while start < len(gems) and end < len(gems):
+        if len(count) < kind:
+            #추가해야함
             end += 1
-        if len(ishere) == cnt:
-            #start 이동
-            # while ishere[gems[start]] > 1:
-            #     print("pz",start)
-            #     ishere[gems[start]] -= 1
-            #     start += 1
-            #     gcnt -= 1
-            # if gcnt == cnt: #최소일 때
-            #     print("?",start,end)
-            #     return [start+1,end]  #더이상 탐색 x
-            if part:
-                if end -1 - start < part[1] - part[0]:
-                    part = [start+1,end]
+            if end == len(gems):
+                break
+            count[gems[end]] += 1
+            
+        elif len(count) == kind:
+            #최적의 답 찾기 배열 줄이기
+            if answer[1] - answer[0] > end - start:
+                answer = [start+1,end+1]
+            #start 옮기기
             else:
-                part = [start+1,end]
-        # print(ishere,gems[start])
-        ishere[g] -= 1
-        gcnt -= 1
-        if ishere[g] == 0:
-            # print(ishere,g)
-            del(ishere[g])
-    return part
-
-#진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간
-#구간은 연속되어야함 (특정범위)
-#[시작 번호, 끝 번호]
-#start 이동 -> start가 이미 구간에 있을 경우
-#end 이동 -> 하나씩 만들어지지 않을 때
+                count[gems[start]] -= 1
+                if count[gems[start]] == 0:
+                    del(count[gems[start]])
+                start += 1
+    return answer
