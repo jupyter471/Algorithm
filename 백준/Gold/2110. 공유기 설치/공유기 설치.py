9408ba1196a7f0db36c94caaab82ccf8152f7c3e
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2024-12-04 21:43:50 +0900

    [Gold IV] Title: 공유기 설치, Time: 5516 ms, Memory: 40412 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,48 +1,30 @@
-#집 n개 공유기 C개 인접한 공유기 거리 최대한 크게 
-#가장 인접한 공유기 거리를 최대한 크게 하게 설치
-#최대 거리 
-import sys
-n,c = map(int,input().split())
-homes = []
+n,c = map(int, input().split())
+house = []
+
 for _ in range(n):
-    homes.append(int(input()))
-homes = sorted(homes)
-ans = []
-#이진탐색 : 특정 수를 찾고싶다 -> 인접한 최대 거리를 찾고 싶다! 
+    house.append(int(input()))
 
-#이진탐색
-start = 0
-end = 1000000000
-if c == 2:
-    print(homes[-1] - homes[0])
-    exit()
+house = sorted(house)
 
-#해당 거리로 공유기 설치 가능한지 체크
-def check(dist,c):
-    src = homes[0]
-    for h in homes[1:]:
-        if src + dist <= h:
-            #dist이상일 때 설치 가능
-            src = h
-            c -= 1
-        if c == 1: #처음에는 무조건 설치
-            ans.append(dist)
-            return True
-    return False
+#구해야하는 것 인접한 공유기의 최대 거리 -> 공유기를 C개만큼 사용하는
 
+start = 1   #공유기 간의 거리 중 최소
+end = house[n-1] - house[0] #공유기 사이의 거리 중 최대
+#최적의 거리를 구해야됨
 while start <= end:
-    mid = (start + end) //2
+    mid = (start + end) // 2
+    #공유기 설치
+    cnt = 1
+    current = house[0]  #첫번째집부터 시작
+    for i in range(1,n):
+        if house[i] >= current + mid:  #공유기간 거리가 mid이상이어야됨 -> 우린 가장 인접한 공유기의 최적 거리를 알고싶으니깐
+            cnt += 1
+            current = house[i]
 
-    if check(mid,c):
-        #설치 가능 -> 거리 더 넓혀서 다시 체크
+    if cnt >= c:
+        #공유기가 부족함-> 간격을 늘려야됨
         start = mid + 1
     else:
-        end = mid - 1
+        end = mid -1
 
-print(max(ans))
-
-
-
-"""
-1 3 7 9 12
-"""
+print(end)
