399cd3bb26b536ef6dfb07f5cda2c83deca6f6b5
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-01-31 23:21:17 +0900

    [D4] Title: 치즈 도둑, Time: 521 ms, Memory: 102,104 KB -BaekjoonHub

---
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        //bfs
+        //무방향 막힐때까지 탐색
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] cheese = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    cheese[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int[] d_r = new int[]{-1,1,0,0};
+            int[] d_c = new int[]{0,0,-1,1};
+            int count = 0;
+
+            //첫째날부터 100일까지 탐색
+            for (int d = 0; d <= 100; d++) {
+                boolean[][] visited = new boolean[N][N];
+                int temp = 0;
+                Deque<int[]> dq = new ArrayDeque<>();
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        if (!visited[i][j]) {
+                            if (cheese[i][j] <= d) {
+                                visited[i][j] = true;
+                                continue;
+                            }
+                            dq.add(new int[]{i,j});
+                            while (!dq.isEmpty()) {
+                                int[] crr = dq.pollFirst();
+                                if (!visited[crr[0]][crr[1]]) {
+                                    if (cheese[crr[0]][crr[1]] <= d) {
+                                        visited[crr[0]][crr[1]] = true;
+                                    }
+                                    else {
+                                        //사방탐색
+                                        visited[crr[0]][crr[1]] = true;
+                                        for (int k = 0; k < 4; k++) {
+                                            int nr = crr[0] + d_r[k];
+                                            int nc = crr[1] + d_c[k];
+                                            if (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+                                                dq.add(new int[]{nr,nc});
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            temp += 1;
+                        }
+                    }
+                }
+                count = Math.max(temp,count);
+            }
+            System.out.printf("#%d %d\n",t,count);
+        }
+    }
+}
