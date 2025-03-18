f69cce390fc72839c1abd6afc142f68fb0e6880e
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-03-19 01:18:33 +0900

    [Gold IV] Title: 공유기 설치, Time: 5408 ms, Memory: 41704 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,30 +1,28 @@
 n,c = map(int, input().split())
-house = []
 
-for _ in range(n):
-    house.append(int(input()))
+num = []
 
-house = sorted(house)
+for i in range(n):
+    num.append(int(input()))
 
-#구해야하는 것 인접한 공유기의 최대 거리 -> 공유기를 C개만큼 사용하는
+num = sorted(num)
 
-start = 1   #공유기 간의 거리 중 최소
-end = house[n-1] - house[0] #공유기 사이의 거리 중 최대
-#최적의 거리를 구해야됨
+start = 1
+end = num[n-1] - num[0]
+
+
 while start <= end:
     mid = (start + end) // 2
-    #공유기 설치
     cnt = 1
-    current = house[0]  #첫번째집부터 시작
+    last = num[0]
     for i in range(1,n):
-        if house[i] >= current + mid:  #공유기간 거리가 mid이상이어야됨 -> 우린 가장 인접한 공유기의 최적 거리를 알고싶으니깐
-            cnt += 1
-            current = house[i]
+        if last + mid <= num[i]:
+            cnt+=1
+            last = num[i]
 
-    if cnt >= c:
-        #공유기가 부족함-> 간격을 늘려야됨
-        start = mid + 1
+    if cnt < c:
+        end = mid - 1
     else:
-        end = mid -1
+        start = mid + 1
 
 print(end)
