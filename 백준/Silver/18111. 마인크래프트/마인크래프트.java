08cc605b524bdf178e1a81101a01e9b39842db36
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-03-19 23:24:18 +0900

    [Silver II] Title: 마인크래프트, Time: 320 ms, Memory: 32644 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
     static int time;
     static int ans;
-    static int[][] map;
+    static int[] freq;
     static int N,M,B;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +14,11 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        freq = new int[257];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                freq[Integer.parseInt(st.nextToken())]++;
             }
         }
 
@@ -30,26 +30,26 @@ public class Main {
         System.out.printf("%d %d\n",time,ans);
     }
 
-    static void trim(int h) {
+    static void trim(int target) {
         int t = 0;
         int add = 0; //새로 쌓아야하는 블록
         int sub = B; //인벤토리 블록 개수
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] > h) {
-                    sub += (map[i][j] - h);
-                    t += ((map[i][j] - h)* 2);
-                }
-                else if (map[i][j] < h) {
-                    add += (h - map[i][j]);
-                    t += (h - map[i][j]);
-                }
+        for (int i = 0; i < 257; i++) {
+            if (target > i) {
+                //더해야함
+                add += (target - i) * freq[i];
+                t += (target - i) * freq[i];
+            }
+            else if (target < i) {
+                //깎아야함
+                sub += (i - target) * freq[i];
+                t += (i - target) * freq[i] * 2;
             }
         }
         if (sub >= add)  {
             if (t < time) {
                 time = t;
-                ans = h;
+                ans = target;
             }
         }
     }
