20ce1348b2561e1e3d73d37e4702a415c903cada
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-03-28 10:21:17 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,4 +1,5 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE FROM ANIMAL_INS
-WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
-ORDER BY ANIMAL_ID;
+select ANIMAL_ID, NAME,SEX_UPON_INTAKE
+from ANIMAL_INS
+where name in ("Lucy", "Ella", "Pickle", "Rogan", "Sabrina", "Mitty")
+order by 1;
