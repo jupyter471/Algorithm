eb9878a5bba16713e6aea354eb36864ea31a1d77
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-05-17 19:34:33 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1604 ms, Memory: 102108 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N,M,R;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        ArrayList[] adj = new ArrayList[N+1];
+        for (int i = 0; i < N+1; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            adj[v].add(u);
+            adj[u].add(v);
+        }
+
+        int[] num = new int[N+1];
+        boolean[] visited = new boolean[N+1];
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.add(R);
+
+        int idx = 1;
+        visited[R] = true;
+
+        while (!dq.isEmpty()) {
+            int c = dq.pollFirst();
+            num[c] = idx++;
+            Collections.sort((ArrayList<Integer>) adj[c]);
+            for (int next : (ArrayList<Integer>)adj[c]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    dq.add(next);
+                }
+            }
+        }
+        for (int i = 1; i < N+1; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
