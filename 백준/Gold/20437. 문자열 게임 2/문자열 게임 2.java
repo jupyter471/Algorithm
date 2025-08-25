b7a330f0df7f64952a52c43ecb92cb544269b79b
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-08-25 12:55:12 +0900

    [Gold V] Title: 문자열 게임 2, Time: 312 ms, Memory: 30868 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             String input = br.readLine();
             int K = Integer.parseInt(br.readLine());
@@ -12,34 +13,32 @@ public class Main {
             int first = Integer.MAX_VALUE;
             int second = -1;
 
-            ArrayList<Integer>[] index = new ArrayList[26];
+            Deque<Integer>[] index = new ArrayDeque[26];
             for (int i = 0; i < 26; i++) {
-                index[i] = new ArrayList<>();
+                index[i] = new ArrayDeque<>();
             }
 
             for (int i = 0; i < input.length(); i++) {
                 int curr = input.charAt(i) - 'a';
                 index[curr].add(i);
-            }
 
-            for (ArrayList ind : index) {
-                if (ind.size() < K) continue;
-
-                for (int start = 0; start + K -1 < ind.size(); start++) {
-                    first = Math.min(first, (Integer) ind.get(start + K -1) - (Integer) ind.get(start) + 1);
-                    second = Math.max(second, (Integer) ind.get(start + K -1) - (Integer) ind.get(start) + 1);
+                if (index[curr].size() == K) {
+                    first = Math.min(first,index[curr].getLast() - index[curr].getFirst()+1);
+                    second = Math.max(second, index[curr].getLast() - index[curr].getFirst() + 1);
+                    index[curr].pollFirst();
                 }
             }
 
             if (first == Integer.MAX_VALUE) {
-                System.out.println(-1);
+                sb.append(-1).append("\n");
                 continue;
             }
             else {
-                System.out.print(first + " ");
+                sb.append(first).append(" ");
             }
 
-            System.out.println(second);
+            sb.append(second).append("\n");
         }
+        System.out.println(sb);
     }
 }
