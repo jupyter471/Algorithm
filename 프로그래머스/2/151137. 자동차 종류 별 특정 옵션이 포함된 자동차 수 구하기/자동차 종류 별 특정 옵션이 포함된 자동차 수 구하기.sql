7d2267d39be08adff940b8df797510fdc9930e92
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-09-27 00:16:45 +0900

    [level 2] Title: 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+# SELECT group_concat(distinct car_type SEPARATOR ' ') 
+# from CAR_RENTAL_COMPANY_CAR
+
+select car_type, count(car_id) as CARS
+from car_rental_company_car
+where options like '%통풍시트%' or options like '%열선시트%' or options like '%가죽시트%'
+group by car_type
+order by 1 
