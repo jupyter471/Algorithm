dde6099276a373440226ddea75c1f9d172f0c5ae
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-09-26 14:52:53 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Main {
     static int N,M,target;
     static ArrayList<Node>[] adjList;
-    static int[] dist;
-
+    static ArrayList<Node>[] reverse;
     public static class Node implements Comparable<Node>{
         int to;
         int cost;
@@ -32,8 +31,10 @@ public class Main {
         target = Integer.parseInt(st.nextToken());
 
         adjList = new ArrayList[N+1];
+        reverse = new ArrayList[N+1];
         for (int i = 1; i <= N; i++) {
             adjList[i] = new ArrayList<>();
+            reverse[i] = new ArrayList<>();
         }
 
 
@@ -43,30 +44,29 @@ public class Main {
             int end = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
             adjList[start].add(new Node(end,time));
+            reverse[end].add(new Node(start, time));  //역방향 그래프 x -> i 돌아오는거
         }
 
-        //다익스트라 N번
+        //다익스트라 2번
         int ans = -1;
-        for (int i = 1; i <= N; i++) {
-            //출발
-            int temp = dij(i,target);
-            //복귀
-            temp += dij(target,i);
+        int[] go = dij(target, adjList);
+        int[] back = dij(target, reverse);
 
-            ans = Math.max(ans,temp);
+        for (int i = 1; i <= N; i++) {
+            ans = Math.max(ans, go[i] + back[i]);
         }
         System.out.println(ans);
     }
 
-    public static int dij(int start, int end) {
-        dist = new int[N+1];
+    public static int[] dij(int start, ArrayList<Node>[] graph) {
+        int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         PriorityQueue<Node> pq = new PriorityQueue<>();
         dist[start] = 0;
         pq.add(new Node(start,0));
         while (!pq.isEmpty()) {
             Node curr = pq.poll();
-            for (Node n : adjList[curr.to]) {
+            for (Node n : graph[curr.to]) {
                 if (dist[curr.to] + n.cost < dist[n.to]) {
                     dist[n.to] = dist[curr.to] + n.cost;
                     pq.add(new Node(n.to, dist[n.to]));
@@ -74,6 +74,6 @@ public class Main {
             }
         }
 
-        return dist[end];
+        return dist;
     }
 }
