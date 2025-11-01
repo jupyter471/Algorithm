7a1d28239b9fee49b2eac958bf036b88e5bb80fc
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-11-01 09:21:35 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,17 +1,21 @@
 -- 
 # select CAR_ID,
 #     case 
-#     when CAR_ID in (select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY where '2022-10-16' between start_date and end_date)
+#     when CAR_ID in (select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY where "2022-10-16" between start_date and end_date)
 #     then '대여중'
-#     else '대여가능'
+#     else '대여 가능'
 #     end AVAILABILITY
 # from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
 # group by 1
 # order by 1 desc
-SELECT CAR_ID,
-CASE WHEN CAR_ID IN (SELECT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY WHERE "2022-10-16" BETWEEN START_DATE AND END_DATE) THEN "대여중"
-ELSE "대여 가능"
-END AVAILABILITY
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-GROUP BY 1
-ORDER BY 1 DESC
+
+-- 
+select car_id,
+    case
+    when car_id in (select car_id from car_rental_company_rental_history where '2022-10-16' between start_date and end_date)
+    then '대여중'
+    else '대여 가능'
+    end AVAILABILITY
+from car_rental_company_rental_history
+group by 1
+order by 1 desc
