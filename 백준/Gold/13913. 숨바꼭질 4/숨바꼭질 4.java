023298872aec2a42736cf8554578333e18f03854
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-11-10 21:01:02 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 3236 ms, Memory: 180948 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int size = 100001;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[size];
+        int[] parent = new int[size];
+        Arrays.fill(arr, -1);
+        Arrays.fill(parent, -1);
+        arr[start] = 0;
+
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        dq.add(start);
+
+        //bfs
+        //x-1, x+1, x * 2
+        List<Integer> nxt;
+        while (!dq.isEmpty()) {
+            int curr = dq.pollFirst();
+            if (curr == target) {
+                break;
+            }
+
+            nxt = List.of(curr - 1, curr + 1, curr * 2);
+            for (int n : nxt) {
+                if (inRange(n) && arr[n] == -1) {
+                    arr[n] = arr[curr] + 1;
+                    parent[n] = curr;
+                    dq.add(n);
+                }
+            }
+        }
+
+        System.out.println(arr[target]);
+
+        //경로 복원
+        Deque<Integer> route = new ArrayDeque<>();
+        int curr = target;
+        while (curr != -1) {
+            route.add(curr);
+            curr = parent[curr];
+        }
+
+        while (!route.isEmpty()) {
+            System.out.printf("%d ", route.pollLast());
+        }
+
+    }
+
+    static boolean inRange(int curr) {
+        return curr >= 0 && curr < size;
+    }
+}
