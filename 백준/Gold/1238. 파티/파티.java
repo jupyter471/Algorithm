7a4be68629c30ee3474a0939f89ec4efe00cf0c9
BokyeongJu <87298145+jupyter471@users.noreply.github.com>
2025-12-01 16:35:51 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,10 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N,M,target;
-    static ArrayList<Node>[] adjList;
-    static ArrayList<Node>[] reverse;
-    public static class Node implements Comparable<Node>{
+    static class Node implements Comparable<Node>{
         int to;
         int cost;
 
@@ -19,57 +16,74 @@ public class Main {
         }
     }
 
+    static ArrayList<ArrayList<Node>> adjList;
+    static ArrayList<ArrayList<Node>> rev;
+    static int N;
+    static int[] in, out;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        //단방향, 왕복 시간
-        //최단거리 중 가장 긴 시간
+        //왔다갔다 왕복
+        adjList = new ArrayList<ArrayList<Node>>();
+        rev = new ArrayList<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); //학생수, 마을수
-        M = Integer.parseInt(st.nextToken());
-        target = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        adjList = new ArrayList[N+1];
-        reverse = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            adjList[i] = new ArrayList<>();
-            reverse[i] = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adjList.add(new ArrayList<Node>());
+            rev.add(new ArrayList<Node>());
         }
-
-
+        
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-            adjList[start].add(new Node(end,time));
-            reverse[end].add(new Node(start, time));  //역방향 그래프 x -> i 돌아오는거
+            int v = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            adjList.get(v).add(new Node(u,cost));
+            rev.get(u).add(new Node(v,cost));
         }
 
-        //다익스트라 2번
-        int ans = -1;
-        int[] go = dij(target, adjList);
-        int[] back = dij(target, reverse);
 
+
+        //다익스트라
+        // 각 집에서 -> X
+        //x -> 각 집
+
+        in = dik(adjList, X);
+        out = dik(rev, X);
+
+        int max = 0;
         for (int i = 1; i <= N; i++) {
-            ans = Math.max(ans, go[i] + back[i]);
+            if (i == X) continue;
+            if (in[i] + out[i] > max) {
+                max = in[i] + out[i];
+            }
         }
-        System.out.println(ans);
+
+        System.out.println(max);
+
     }
 
-    public static int[] dij(int start, ArrayList<Node>[] graph) {
+    static int[] dik(ArrayList<ArrayList<Node>> adj, int start) {
         int[] dist = new int[N+1];
+        boolean[] v = new boolean[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        PriorityQueue<Node> pq = new PriorityQueue<>();
         dist[start] = 0;
-        pq.add(new Node(start,0));
-        while (!pq.isEmpty()) {
-            Node curr = pq.poll();
-            for (Node n : graph[curr.to]) {
-                if (dist[curr.to] + n.cost < dist[n.to]) {
-                    dist[n.to] = dist[curr.to] + n.cost;
-                    pq.add(new Node(n.to, dist[n.to]));
+
+        PriorityQueue<Node> dq = new PriorityQueue<>();
+        dq.add(new Node(start, 0));
+
+        while (!dq.isEmpty()) {
+            Node curr = dq.poll();
+            if (v[curr.to]) continue;
+            v[curr.to] = true;
+            for (int i = 0; i < adj.get(curr.to).size(); i++) {
+                if (dist[curr.to] + adj.get(curr.to).get(i).cost < dist[adj.get(curr.to).get(i).to]) {
+                    dist[adj.get(curr.to).get(i).to] = dist[curr.to] + adj.get(curr.to).get(i).cost;
+                    dq.add(new Node(adj.get(curr.to).get(i).to, dist[curr.to] + adj.get(curr.to).get(i).cost));
                 }
             }
         }
